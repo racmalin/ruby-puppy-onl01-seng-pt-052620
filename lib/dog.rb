8e56debb2c502f2d8d436b31b1eecb9743595cf1
Kevin C Malin <racmalin@yahoo.com>
2020-06-11 16:17:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,29 @@
 class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = [] 
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
 end
+
+  def save
+    @@all << self
+  end
+
+  def name
+    @name
+  end
